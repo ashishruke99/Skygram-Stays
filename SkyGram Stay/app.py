60de222a654af1311bca2414c270f67213edd9b3
Harshvardhan-Kulkarni <152486669+Harshvardhan-Kulkarni@users.noreply.github.com>
2024-08-17 21:15:00 +0530

    Update app.py

--- SkyGram Stay/app.py
+++ SkyGram Stay/app.py
@@ -64,4 +64,5 @@ def create_account():
     return 'Create New Account page - To be implemented'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+init_db()
+app.run(host='0.0.0.0', port=5000, debug=True)
